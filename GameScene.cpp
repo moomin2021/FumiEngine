cd9ab06e120e722ff0fc758b133979add671628d
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-11-29 09:22:05 +0900

    完了

--- GameScene.cpp
+++ GameScene.cpp
@@ -2,6 +2,7 @@
 
 // コンストラクタ
 GameScene::GameScene() :
+	key_(nullptr),// -> キー入力
 	sprite_(nullptr)// -> スプライト
 {
 
@@ -14,17 +15,32 @@ GameScene::~GameScene() {
 
 // 初期化処理
 void GameScene::Initialize() {
+	// インスタンス取得
+	key_ = Key::GetInstance();
+
 	// スプライト
 	sprite_ = new Sprite();
+
+	// 描画フラグ
+	isDraw_ = false;
 }
 
 // 更新処理
 void GameScene::Update() {
+	// [SPACE]キーを押したら描画フラグを切り替える
+	if (key_->TriggerKey(DIK_SPACE)) isDraw_ = !isDraw_;
+
+	// スプライト移動処理
+	sprite_->position.x += key_->PushKey(DIK_D) - key_->PushKey(DIK_A);
+	sprite_->position.y += key_->PushKey(DIK_S) - key_->PushKey(DIK_W);
+
+	// スプライト更新処理
 	sprite_->Update();
 }
 
 // 描画処理
 void GameScene::Draw() {
 	Sprite::PreDraw();
-	sprite_->Draw();
+	// 描画フラグが[ON]なら描画
+	if (isDraw_) sprite_->Draw();
 }
--- GameScene.h
+++ GameScene.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "BaseScene.h"// -> シーンのベース
 #include "Sprite.h"// -> スプライト
+#include "Key.h"// -> キーボード入力
 
 class GameScene : public BaseScene {
 	// --メンバ変数-- //
 public:
 
 private:
+	// キーボード入力クラス
+	Key* key_;
+
 	// スプライト
 	Sprite* sprite_;
+
+	// 描画フラグ
+	bool isDraw_;
 
 	// --メンバ関数-- //
 public:
