0e90dc1f3181c0ddbde25c53944411879a3a7e6a
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-07-04 17:07:49 +0900

    AL4の04_03まで終了

---
+++ Engine/Collision/CollisionPrimitive.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <DirectXMath.h>
+
+// 球
+struct Sphere {
+	// 中心座標
+	DirectX::XMVECTOR center = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+	// 半径
+	float radius = 1.0f;
+};
+
+// 平面
+struct Plane {
+	// 法線ベクトル
+	DirectX::XMVECTOR normal = { 0.0f, 1.0f, 0.0f, 0.0f };
+
+	// 原点(0.0f, 0.0f, 0.0f)からの距離
+	float distance = 0.0f;
+};
+
+// 三角形
+class Triangle {
+public:
+	// 頂点座標3つ
+	DirectX::XMVECTOR p0;
+	DirectX::XMVECTOR p1;
+	DirectX::XMVECTOR p2;
+
+	// 法線ベクトル
+	DirectX::XMVECTOR normal;
+
+	void ComputeNormal();
+};
+
+struct Ray {
+	// 始点座標
+	DirectX::XMVECTOR start = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+	// 方向
+	DirectX::XMVECTOR dir = { 1.0f, 0.0f, 0.0f, 0.0f };
+};
