2b5013bf468a8727d303d5bf139bc81b16de05b7
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-07-11 10:45:40 +0900

    メッシュ分け完了

--- Engine/3d/Model.h
+++ Engine/3d/Model.h
@@ -1,7 +1,9 @@
 #pragma once
-
+#include "Mesh.h"
+#include "Material.h"
 
 #include <vector>
+#include <map>
 #include <d3d12.h>
 #include <wrl.h>
 
@@ -12,22 +14,11 @@ class Model {
 
 #pragma region メンバ変数
 private:
-	// 頂点データ
-	std::vector<Vertex> vertex_;			// 頂点データ
-	D3D12_VERTEX_BUFFER_VIEW vertexView_;	// 頂点バッファービュー
-	ComPtr<ID3D12Resource> vertexBuff_;		// 頂点バッファ
-
-	// インデックスデータ
-	std::vector<uint16_t> index_;		// インデックスデータ
-	D3D12_INDEX_BUFFER_VIEW indexView_;	// インデックスバッファビュー
-	ComPtr<ID3D12Resource> indexBuff_;	// インデックスバッファ
+	// メッシュデータ
+	std::vector<Mesh> meshes_;
 
 	// マテリアルデータ
-	Material material_;						// マテリアルデータ
-	ComPtr<ID3D12Resource> materialBuff_;	// マテリアルバッファ
-
-	// テクスチャハンドル
-	uint16_t textureHandle_;
+	std::map<std::string, Material> materials_;
 #pragma endregion
 
 #pragma region メンバ関数
@@ -56,20 +47,5 @@ private:
 	/// <param name="directoryPath"> ファイル名 </param>
 	/// <param name="fileName"> ファイル名 </param>
 	void LoadMaterial(const std::string& directoryPath, const std::string& fileName);
-
-	/// <summary>
-	/// 頂点バッファ作成
-	/// </summary>
-	void CreateVertexBuff();
-
-	/// <summary>
-	/// インデックスバッファ作成
-	/// </summary>
-	void CreateIndexBuff();
-
-	/// <summary>
-	/// マテリアルバッファ作成
-	/// </summary>
-	void CreateMaterialBuff();
 #pragma endregion
 };
