3fd4c649b0662bb8690a1a6a4378e456c8e37b60
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-07-11 10:57:26 +0900

    masterとマージ

---
+++ Engine/3d/Mesh.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "float2.h"
+#include "float3.h"
+
+#include <d3d12.h>
+#include <wrl.h>
+#include <string>
+#include <vector>
+
+class Mesh
+{
+#pragma region エイリアステンプレート
+private:
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+#pragma endregion
+
+#pragma region 構造体
+public:
+	// 頂点データ
+	struct Vertex {
+		float3 pos;		// 座標(XYZ)
+		float3 normal;	// 法線(XYZ)
+		float2 uv;		// UV座標(XY)
+	};
+#pragma endregion
+
+#pragma region メンバ変数
+private:
+	// 頂点データ
+	std::vector<Vertex> vertices_;			// 頂点データ
+	D3D12_VERTEX_BUFFER_VIEW vertexView_;	// 頂点バッファービュー
+	ComPtr<ID3D12Resource> vertexBuff_;		// 頂点バッファ
+
+	// インデックスデータ
+	std::vector<uint16_t> indexes_;		// インデックスデータ
+	D3D12_INDEX_BUFFER_VIEW indexView_;	// インデックスバッファビュー
+	ComPtr<ID3D12Resource> indexBuff_;	// インデックスバッファ
+
+	// 適用されるマテリアルの名前
+	std::string materialName_;
+#pragma endregion
+
+#pragma region メンバ関数
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Mesh();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// 頂点データを追加
+	/// </summary>
+	/// <param name="vertex"> 頂点データ </param>
+	void AddVertex(const Vertex& vertex) { vertices_.emplace_back(vertex); }
+
+	/// <summary>
+	/// インデックスデータを追加
+	/// </summary>
+	/// <param name="index"> インデックスデータ </param>
+	void AddIndex(uint16_t index) { indexes_.emplace_back(index); }
+
+	/// <summary>
+	/// 頂点バッファ作成
+	/// </summary>
+	void CreateVertexBuff();
+
+	/// <summary>
+	/// インデックスバッファ作成
+	/// </summary>
+	void CreateIndexBuff();
+#pragma endregion
+
+#pragma region セッター関数
+public:
+	/// <summary>
+	/// マテリアルの名前を設定
+	/// </summary>
+	/// <param name="name"> マテリアルの名前 </param>
+	void SetMaterialName(const std::string& name) { materialName_ = name; }
+#pragma endregion
+
+#pragma region ゲッター関数
+	/// <summary>
+	/// マテリアルの名前を取得
+	/// </summary>
+	/// <returns> マテリアルの名前 </returns>
+	const std::string& GetMaterialName() { return materialName_; }
+#pragma endregion
+};
