5852c3abc5f9dec377617421b7f56f66438cb717
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-07-13 15:05:21 +0900

    メッシュ対応

--- Engine/Collision/CollisionManager.cpp
+++ Engine/Collision/CollisionManager.cpp
@@ -1,6 +1,7 @@
 #include "CollisionManager.h"
 #include "CollisionPrimitive.h"
 #include "Collision.h"
+#include "MeshCollider.h"
 
 CollisionManager::~CollisionManager()
 {
@@ -29,11 +30,35 @@ void CollisionManager::CheckAllCollision()
 			BaseCollider* colB = *itB;
 
 			// ともに球
-			if (colA->GetShapeType() == SHAPE_SPHERE && colA->GetShapeType() == SHAPE_SPHERE) {
+			if (colA->GetShapeType() == SHAPE_SPHERE && colB->GetShapeType() == SHAPE_SPHERE) {
 				Sphere* sphereA = dynamic_cast<Sphere*>(colA);
 				Sphere* sphereB = dynamic_cast<Sphere*>(colB);
 				Vector3 inter;
 				if (Collision::CheckSphere2Sphere(*sphereA, *sphereB, &inter)) {
+					colA->SetIsHit(true);
+					colB->SetIsHit(true);
+				}
+			}
+
+			// メッシュと球
+			else if (colA->GetShapeType() == SHAPE_MESH && colB->GetShapeType() == SHAPE_SPHERE) {
+				MeshCollider* meshCollider = dynamic_cast<MeshCollider*>(colA);
+				Sphere* sphere = dynamic_cast<Sphere*>(colB);
+				Vector3 inter;
+
+				if (meshCollider->CheckCollisionSphere(*sphere, &inter)) {
+					colA->SetIsHit(true);
+					colB->SetIsHit(true);
+				}
+			}
+
+			// 球とメッシュ
+			else if (colA->GetShapeType() == SHAPE_SPHERE && colB->GetShapeType() == SHAPE_MESH) {
+				MeshCollider* meshCollider = dynamic_cast<MeshCollider*>(colB);
+				Sphere* sphere = dynamic_cast<Sphere*>(colA);
+				Vector3 inter;
+
+				if (meshCollider->CheckCollisionSphere(*sphere, &inter)) {
 					colA->SetIsHit(true);
 					colB->SetIsHit(true);
 				}
