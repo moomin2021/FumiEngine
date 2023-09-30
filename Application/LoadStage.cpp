f610268761f269212b96d8503a78dcaf2fabe0ef
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-09-30 15:00:00 +0900

    ステージ読み込み用のクラスを作成

---
+++ Application/LoadStage.cpp
@@ -0,0 +1,123 @@
+#include "LoadStage.h"
+
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <json.hpp>
+
+LoadStage::LoadStage() {}
+
+LoadStage::~LoadStage() {}
+
+void LoadStage::Load(std::string fileName)
+{
+	// ファイルストリーム
+	std::ifstream file;
+
+	// ファイルを開く
+	file.open(fileName);
+
+	// ファイルオープン失敗チェック
+	if (file.fail()) assert(0);
+
+	// JSON文字列から回答したデータ
+	nlohmann::json deserialized;
+
+	// 解凍
+	file >> deserialized;
+
+	// 正しいレベルエディタファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	// "name"を文字列として取得
+	std::string name = deserialized["name"].get<std::string>();
+
+	// 正しいレベルデータファイルかチェック
+	assert(name.compare("scene") == 0);
+
+	// レベルデータ格納用インスタンスを生成
+	LevelData* levelData = new LevelData();
+
+	// "objects"の全オブジェクトを走査
+	for (nlohmann::json& object : deserialized["objects"]) {
+		assert(object.contains("type"));
+
+		// 種別を取得
+		std::string type = object["type"].get<std::string>();
+
+		// MESH
+		if (type.compare("MESH") == 0) {
+			// 要素追加
+			levelData->objects.emplace_back(LevelData::ObjectData{});
+
+			// 今追加した要素の参照を得る
+			LevelData::ObjectData& objectData = levelData->objects.back();
+
+			// モデル
+			if (object.contains("file_name")) {
+				// ファイル名
+				objectData.fileName = object["file_name"];
+
+				// 同じファイル名のモデルが無かったらモデル追加
+				if (models_.count(object["file_name"]) == 0) {
+					models_.emplace(object["file_name"], std::make_unique<Model>(object["file_name"]));
+				}
+			}
+
+			// クラスの名前
+			if (object.contains("class_name")) {
+				objectData.className = object["class_name"];
+			}
+
+			// visible(表示批評)
+			//objectData.isActive = object["visible"];
+
+			// ｔランスフォームのパラメータ読み込み
+			nlohmann::json& transform = object["transform"];
+
+			// 平行移動
+			objectData.translation.x = (float)transform["translation"][0];
+			objectData.translation.y = (float)transform["translation"][2];
+			objectData.translation.z = (float)transform["translation"][1];
+
+			// 回転角
+			objectData.rotation.x = -(float)transform["rotation"][1];
+			objectData.rotation.y = -(float)transform["rotation"][2];
+			objectData.rotation.z = (float)transform["rotation"][0];
+
+			// スケーリング
+			objectData.scaling.x = (float)transform["scaling"][0];
+			objectData.scaling.y = (float)transform["scaling"][2];
+			objectData.scaling.z = (float)transform["scaling"][1];
+		}
+
+		// オブジェクト走査を再帰関数にまとめ、再帰呼出で枝を走査する
+		if (object.contains("children")) {
+
+		}
+	}
+
+	// レベルデータからオブジェクトを生成、配置
+	for (auto& objectData : levelData->objects) {
+		if (objectData.className == "Enemy") {
+			// 敵を追加
+			//enemyMgr_->CreateAddEnemy0(objectData.translation, objectData.scaling);
+		}
+
+		else if (objectData.className == "Item") {
+			// アイテムを追加
+			//ItemManager::GetInstace()->AddItem(objectData.translation);
+		}
+
+		else if (objectData.className == "BossGenerator") {
+			//enemyMgr_->SetBossGenerator(objectData.translation);
+		}
+
+		else {
+			// オブジェクト追加
+			//stageMgr_->AddObject3D(objectData.fileName, objectData.translation, objectData.rotation, objectData.scaling);
+		}
+	}
+}
