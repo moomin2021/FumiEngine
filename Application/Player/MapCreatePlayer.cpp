b03a69edc1657c6ff9e6da5e661af198ea13b3b3
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-10-31 16:48:16 +0900

    マップ作成シーン 途中

--- Application/Player/MapCreatePlayer.cpp
+++ Application/Player/MapCreatePlayer.cpp
@@ -5,28 +5,55 @@
 
 MapCreatePlayer::~MapCreatePlayer()
 {
+
 }
 
 void MapCreatePlayer::Initialize()
 {
+
 }
 
 void MapCreatePlayer::Update()
 {
+
 }
 
 void MapCreatePlayer::Draw()
 {
+
 }
 
 void MapCreatePlayer::OnCollision()
 {
+
 }
 
 void MapCreatePlayer::MatUpdate()
 {
+
 }
 
 void MapCreatePlayer::Debug()
 {
+
+}
+
+void (MapCreatePlayer::* MapCreatePlayer::stateTable[]) () = {
+	&MapCreatePlayer::Normal,	// 通常状態
+	&MapCreatePlayer::Fly,		// 空中状態
+};
+
+void MapCreatePlayer::Normal()
+{
+
+}
+
+void MapCreatePlayer::Fly()
+{
+
+}
+
+void MapCreatePlayer::Fall()
+{
+
 }
--- Application/Player/MapCreatePlayer.h
+++ Application/Player/MapCreatePlayer.h
@@ -5,10 +5,24 @@
 
 class MapCreatePlayer
 {
+private:
+	enum class State {
+		NORMAL,
+		AIR,
+	};
+
 #pragma region メンバ変数
 private:
 	// カメラ
 	Camera* camera_ = nullptr;
+
+	// 状態
+	State state_ = State::NORMAL;
+
+	// 重力
+	float yVel_ = 0.0f;				// y方向の速度
+	const float gravityAcc_ = -0.1f;// 重力加速度
+	const float jumpSpd_ = 1.0f;	// ジャンプ速度
 #pragma endregion
 
 #pragma region メンバ関数
@@ -33,9 +47,18 @@ public:
 
 	// デバック処理
 	void Debug();
+
+private:
+	// 状態別処理
+	static void (MapCreatePlayer::* stateTable[]) ();
+	void Normal();	// 通常状態
+	void Fly();		// 飛翔状態
+
+	void Fall();	// 落下処理
 #pragma endregion
 
 #pragma region セッター関数
+public:
 	void SetCamera(Camera* pCamera) { camera_ = pCamera; }
 #pragma endregion
 };
